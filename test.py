0f7394da07236a85bf29059db69eb63e3c6f1edd
sigurdurhaukur <56935611+sigurdurhaukur@users.noreply.github.com>
2023-05-19 23:29:30 +0000

    stuff

--- test.py
+++ test.py
@@ -3,30 +3,32 @@ import pandas as pd
 import time
 #n+1 = 43205
 # max_requests = 43205
-max_requests = 20 + 1
+max_requests = 1000 + 1
 
 dates = []
-x = requests.get("https://map.is/webservice/proxies/usQueryHTML.php?nid=1")
 
 start_time = time.time()
-for i in range(1,max_requests):
-    url = "https://map.is/webservice/proxies/usQueryHTML.php?nid=" + str(i)
-    x = requests.get(url)
-    print("trying", url, "dagsetning" in x.text)
-    if ("dagsetning" in x.text):
-        texti = x.text
+for i in range(140,max_requests):
+    try:
+        url = "https://map.is/webservice/proxies/usQueryHTML.php?nid=" + str(i)
+        x = requests.get(url)
+        print("trying", url, "dagsetning" in x.text)
 
-        date = texti[texti.find("dagsetning")+14:texti.find("dagsetning")+21    ] #Slicear út dagsetninguna YYYY-MM
+        if ("dagsetning" in x.text):
+            texti = x.text
 
-        # print("Date: " + date)
+            date = texti[texti.find("dagsetning")+14:texti.find("dagsetning")+21    ] #Slicear út dagsetninguna YYYY-MM
 
-        if (i == 3):
-            previous_date = dates[i]
-            print(previous_date, date, previous_date == date)
+            # print("Date: " + date)
+
+            dates.append(date)
+
+            previous_date = dates[i - 1]
+            print(i, previous_date, date, previous_date != date)
             if(previous_date != date):
                 # new date
                 accidents_amount = len(dates)
-                dates = []
+                # dates = []
 
                 data = data.append({previous_date: accidents_amount})
 
@@ -35,8 +37,11 @@ for i in range(1,max_requests):
                 f.close()
 
                 print("Date: " + date + " Accidents: " + str(accidents_amount))
-
-        dates.append(date)
+    except Exception as e:
+        if e == KeyboardInterrupt:
+            break
+        print("Error")
+        pass
 
 
 end_time = time.time()
