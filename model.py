27a14b89322da9d8a933c3b7b770334841a5593b
sigurdurhaukur <56935611+sigurdurhaukur@users.noreply.github.com>
2023-05-19 22:02:25 +0000

    some help from Bjarni

--- model.py
+++ model.py
@@ -18,18 +18,18 @@ accidents = [10, 15, 8, 12, 11, 13]
 X = torch.tensor(list(zip(years, months, temperatures)), dtype=torch.float32)
 y = torch.tensor(accidents, dtype=torch.float32)
 
-# Normalize the input features
+# Standardize the input features
 X_mean = torch.mean(X, dim=0)
 X_std = torch.std(X, dim=0)
-X_normalized = (X - X_mean) / X_std
+X_standardized = (X - X_mean) / X_std
 
 
 
 
 # Define the model
-class PolynomialRegression(nn.Module):
+class LinearRegression(nn.Module):
     def __init__(self):
-        super(PolynomialRegression, self).__init__()
+        super(LinearRegression, self).__init__()
         self.linear = nn.Linear(3, 1)  # 3 input features and 1 output
 
     def forward(self, x):
@@ -38,7 +38,7 @@ class PolynomialRegression(nn.Module):
 
 
 # Initialize the model
-model = PolynomialRegression()
+model = LinearRegression()
 criterion = nn.MSELoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01)
 
@@ -50,7 +50,7 @@ num_epochs = 1000
 
 for epoch in range(num_epochs):
     # Forward pass
-    outputs = model(X_normalized)
+    outputs = model(X_standardized)
     loss = criterion(outputs, y)
 
     # Backward and optimize
@@ -65,12 +65,9 @@ for epoch in range(num_epochs):
 
 
 # inference
-new_data = torch.tensor([[2021, 7, 27.3]], dtype=torch.float32) # 27.3 degrees on July 2021
-normalized_new_data = (new_data - X_mean) / X_std
-predicted = model(normalized_new_data)
+new_data = torch.tensor([[2023, 7, 27.3]], dtype=torch.float32) # 27.3 degrees on July 2023
+standardized_new_data = (new_data - X_mean) / X_std
+predicted = model(standardized_new_data)
 
 # Print the predicted amount of accidents
 print(f'Predicted amount of accidents: {predicted.item():.2f}')
-
-
-
