9ae313aa92b7ee47a6bb311e20b12ac312aac896
sigurdurhaukur <56935611+sigurdurhaukur@users.noreply.github.com>
2023-05-20 19:18:42 +0000

    final final

--- usa/preprocessing.py
+++ usa/preprocessing.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
 # Load the data
-data = pd.read_csv(
-    "./US-data.csv"
-)  # Replace "your_data.csv" with the path to your dataset
+data = pd.read_csv("./US-data.csv")
 
 # Preprocess the data
 data["Start_Time"] = pd.to_datetime(data["Start_Time"])  # Convert to datetime if needed
@@ -12,29 +10,33 @@ temperature = data["Temperature(C)"].round()
 wind_speed = data["Wind_Speed(m/s)"].round()
 
 # Define the bins for temperature and wind speed
-temperature_bins = [-float("inf"), 0, 5, 10, 15, 20, 25, 30, 35, float("inf")]
-wind_speed_bins = [-float("inf"), 0, 5, 10, 15, 20, 25, 30, 35, float("inf")]
+temperature_bins = [-float("inf"), -5, 0, 5, 10, 15, 20, 25, 30, 35, float("inf")]
+wind_speed_bins = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, float("inf")]
 
 # Assign labels to each bin
 temperature_labels = [
     "Extremely Cold",
     "Very Cold",
-    "Slightly Cold",
     "Cold",
-    "Moderate",
+    "Slightly Cold",
+    "Moderate Cold",
+    "Moderate Hot",
     "Slightly Hot",
+    "Hot",
     "Very Hot",
     "Extremely Hot",
 ]
 wind_speed_labels = [
-    "Extremely low",
-    "Very low",
-    "Slightly low",
-    "low",
-    "Moderate",
-    "Slightly high",
-    "Very high",
-    "Extremely high",
+    "No Wind",
+    "Very Low",
+    "Low",
+    "Slightly Low",
+    "Moderate Low",
+    "Moderate High",
+    "Slightly High",
+    "High",
+    "Very High",
+    "Extremely High",
 ]
 
 # Group the data by binned temperature and wind speed and count the number of accidents
@@ -53,5 +55,5 @@ grouped_data = (
 
 print(grouped_data)
 
-# save as csv file
+# Save as a CSV file
 grouped_data.to_csv("processed-usa-data.csv", index=False)
