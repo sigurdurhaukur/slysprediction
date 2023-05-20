ae814e989a51d9c5715c991b13cfae223281dba3
Tomas Blaer Gudmundsson <tomasblaer@gmail.com>
2023-05-20 16:48:22 +0000

    csvread touchup

--- accidents/accidentcount.py
+++ accidents/accidentcount.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math as m
 
-filename = ""
+'''
 hiti = {}
 vindur = {}
 def init():
@@ -9,16 +9,35 @@ def init():
         hiti[i] = 0
     for j in range(1,50):
         vindur[j] = 0
-
+'''
+        
 vindur = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0, 13: 0, 14: 0, 15: 0, 16: 0, 17: 0, 18: 0, 19: 0, 20: 0, 21: 0, 22: 0, 23: 0, 24: 0, 25: 0, 26: 0, 27: 0, 28: 0, 29: 0, 30: 0, 31: 0, 32: 0, 33: 0, 34: 0, 35: 0, 36: 0, 37: 0, 38: 0, 39: 0, 40: 0, 41: 0, 42: 0, 43: 0, 44: 0, 45: 0, 46: 0, 47: 0, 48: 0, 49: 0}
 hiti = {-45: 0, -44: 0, -43: 0, -42: 0, -41: 0, -40: 0, -39: 0, -38: 0, -37: 0, -36: 0, -35: 0, -34: 0, -33: 0, -32: 0, -31: 0, -30: 0, -29: 0, -28: 0, -27: 0, -26: 0, -25: 0, -24: 0, -23: 0, -22: 0, -21: 0, -20: 0, -19: 0, -18: 0, -17: 0, -16: 0, -15: 0, -14: 0, -13: 0, -12: 0, -11: 0, -10: 0, -9: 0, -8: 0, -7: 0, -6: 0, -5: 0, -4: 0, -3: 0, -2: 0, -1: 0, 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0, 13: 0, 14: 0, 15: 0, 16: 0, 17: 0, 18: 0, 19: 0, 20: 0, 21: 0, 22: 0, 23: 0, 24: 0, 25: 0, 26: 0, 27: 0, 28: 0, 29: 0, 30: 0, 31: 0, 32: 0, 33: 0, 34: 0, 35: 0, 36: 0, 37: 0, 38: 0, 39: 0, 40: 0, 41: 0, 42: 0, 43: 0, 44: 0, 45: 0, 46: 0, 47: 0, 48: 0, 49: 0}
+filename = ""
 
 def readcsv(file):
-    (d,h,v) = np.loadtxt(filename,delimiter=",").T
+    (d,h,v) = np.loadtxt(filename,dtype=str,delimiter=",").T
     dags = d.tolist()
     hitastig = h.astype(int)
-    vindhradi = v.astype(int)
-    for i in hitastig:
-        hiti[m.ceil(i)] = hiti[m.ceil(i)]+1
-    for j in vindhradi:
-        vindur[m.ceil(j)] = vindur[m.ceil(j)]+1
+    vindhradi = v.astype(float)
+    try:
+        for i in hitastig:
+            hiti[i] = hiti[i]+1
+        print("Done populating temp dictionary")
+        for j in vindhradi:
+            vindur[j] = vindur[j]+1
+        print("Done populating wind dictionary")
+    except:
+        print("error occured, writing to txt file")
+        with open("data.txt","w") as f:
+            f.write(vindur)
+            f.write(hiti)
+
+
+
+
+
+    
+readcsv(filename)
+print(vindur)
+print(hiti)
